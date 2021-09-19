2c9513fee2de568e540e151954299d6fece55995
Nohely Badillo <62182593+nohelyx-bc@users.noreply.github.com>
2021-09-19 18:21:04 -0500

    Add const and let with parameters for the squares of the Tetris

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-document.addEventListener('DOMContentLoaded' () => {
-    
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+
+    //The tetrominoes
+    const lTetromino = [
+        [1, width+1, width+2+1, 2],
+        [width, width+1, width+2, width+2+2],
+        [1, width+1, width+2+1, width+2],
+        [width, width+2, width+2+1, width+2+2]
+    ]
+
 })
